867ce964680d991eb2411d819445b38425b8dce0
Yadu Krishna <yaduk946@gmail.com>
2024-01-30 22:36:52 +0530

    added button

--- client/src/Components/login.jsx
+++ client/src/Components/login.jsx
@@ -12,7 +12,7 @@ import LoginUserSelectButton from "./LoginUserSelectButton.jsx";
 const Login = () => {
   const [user, setUser] = useState("");
   const [errors, setError] = useState([]);
-  const [userType, setuserType] = useState("Student");
+  const [userType, setuserType] = useState();
 
   const [formData, setFormData] = useState({
     email: "",
@@ -59,26 +59,30 @@ const Login = () => {
           </div>
         </div> */}
         <LoginUserSelectButton setuserType={setuserType} />
-        <div className="flex justify-center items-center">
-          <div className="flex h-auto xl:w-2/3  ">
-            <div className="">
-              <img className="hidden lg:block rounded-l-lg" src={loginimage} />
-            </div>
-            <div className="bg-cyan-600 p-8 rounded-r-lg">
-              <Card color="transparent" shadow={false} className="">
-                <div className=" flex justify-center ">
-                  <Typography variant="h2" color="blue-gray" className="">
-                    {`${userType} `}Sign In
-                  </Typography>
-                </div>
+        {userType && (
+          <div className="flex justify-center items-center">
+            <div className="flex h-auto xl:w-2/3  ">
+              <div className="">
+                <img
+                  className="hidden lg:block rounded-l-lg"
+                  src={loginimage}
+                />
+              </div>
+              <div className="bg-cyan-600 p-8 rounded-r-lg">
+                <Card color="transparent" shadow={false} className="">
+                  <div className=" flex justify-center ">
+                    <Typography variant="h2" color="blue-gray" className="">
+                      {userType && `${userType} `}Sign In
+                    </Typography>
+                  </div>
 
-                <form
-                  onSubmit={handleSubmit}
-                  className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96"
-                >
-                  <div className="mb-1 flex flex-col gap-7">
-                    <div className="relative inline-block">
-                      {/* <select
+                  <form
+                    onSubmit={handleSubmit}
+                    className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96"
+                  >
+                    <div className="mb-1 flex flex-col gap-7">
+                      <div className="relative inline-block">
+                        {/* <select
                       onChange={(e) => setUser(e.target.value)}
                       className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-2 px-4 pr-8 rounded-lg leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                     >
@@ -87,58 +91,59 @@ const Login = () => {
                       <option value="Staff">Staff</option>
                       <option value="Master">Master</option>
                     </select> */}
-                      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                        <svg
-                          className="w-4 h-4"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                        >
-                          <path
-                            fill-rule="evenodd"
-                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 011.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                            clip-rule="evenodd"
-                          />
-                        </svg>
+                        <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                          <svg
+                            className="w-4 h-4"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                          >
+                            <path
+                              fill-rule="evenodd"
+                              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 011.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                              clip-rule="evenodd"
+                            />
+                          </svg>
+                        </div>
                       </div>
+
+                      <Input
+                        size="lg"
+                        name="email"
+                        placeholder="Enter Your Mail"
+                        onChange={handleInputChange}
+                        className=" border-t-blue-gray-200 focus:!border-t-gray-900 p-3 rounded-md"
+                        labelProps={{
+                          className: "before:content-none after:content-none",
+                        }}
+                      />
+
+                      <Input
+                        type="password"
+                        size="lg"
+                        name="password"
+                        placeholder="Enter Password"
+                        onChange={handleInputChange}
+                        className=" !border-t-blue-gray-200 focus:!border-t-gray-900 p-3 rounded-md"
+                        labelProps={{
+                          className: "before:content-none after:content-none",
+                        }}
+                      />
                     </div>
 
-                    <Input
-                      size="lg"
-                      name="email"
-                      placeholder="Enter Your Mail"
-                      onChange={handleInputChange}
-                      className=" border-t-blue-gray-200 focus:!border-t-gray-900 p-3 rounded-md"
-                      labelProps={{
-                        className: "before:content-none after:content-none",
-                      }}
-                    />
-
-                    <Input
-                      type="password"
-                      size="lg"
-                      name="password"
-                      placeholder="Enter Password"
-                      onChange={handleInputChange}
-                      className=" !border-t-blue-gray-200 focus:!border-t-gray-900 p-3 rounded-md"
-                      labelProps={{
-                        className: "before:content-none after:content-none",
-                      }}
-                    />
-                  </div>
-
-                  <Button
-                    className="mt-6 w-full bg-red-400 hover:bg-red-500  p-2 font-semibold text-white rounded-md"
-                    fullWidth
-                    type="submit"
-                  >
-                    sign up
-                  </Button>
-                </form>
-              </Card>
+                    <Button
+                      className="mt-6 w-full bg-red-400 hover:bg-red-500  p-2 font-semibold text-white rounded-md"
+                      fullWidth
+                      type="submit"
+                    >
+                      sign up
+                    </Button>
+                  </form>
+                </Card>
+              </div>
             </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
