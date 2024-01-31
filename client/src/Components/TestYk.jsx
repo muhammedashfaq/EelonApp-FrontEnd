e123796326bbc0f77d0a3f366d9f2acbaaaf7b7e
Yadu Krishna <yaduk946@gmail.com>
2024-01-31 12:54:25 +0530

    added student pay history table

--- client/src/Components/TestYk.jsx
+++ client/src/Components/TestYk.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import StudentFeeInvoicesComponent from "./Student/DashboardComponent/StudentFeeInvoicesComponent";
 import StudentDetails from "./Student/DashboardComponent/StudentDetails";
 import { StudentFeeStructure } from "./Student/DashboardComponent/StudentFeeStructure";
+import { StudentPaymentHistory } from "./Student/DashboardComponent/StudentPaymentHistory";
 
 const TestYk = () => {
   return (
     <>
       {/* <StudentDetails /> */}
       {/* <StudentFeeInvoicesComponent /> */}
-      <StudentFeeStructure />
+      {/* <StudentFeeStructure /> */}
+      <StudentPaymentHistory />
     </>
   );
 };
