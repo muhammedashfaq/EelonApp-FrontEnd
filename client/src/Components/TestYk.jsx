336e3c332b31b75d1a8c4549283be142d0135e3e
Yadu Krishna <yaduk946@gmail.com>
2024-02-01 12:50:26 +0530

    Created  addbooksModal

--- client/src/Components/TestYk.jsx
+++ client/src/Components/TestYk.jsx
@@ -1,27 +1,12 @@
+import React from "react";
+import { Button, Checkbox, Input } from "@material-tailwind/react";
+import LibraryBooksAddModal from "./Staff/DashboardComponents/LibraryBooksAddModal";
 
-// import React from "react";
-// import StudentFeeInvoicesComponent from "./Student/DashboardComponent/StudentFeeInvoicesComponent";
-// import StudentDetails from "./Student/DashboardComponent/StudentDetails";
-// import { StudentFeeStructure } from "./Student/DashboardComponent/StudentFeeStructure";
-// import { StudentPaymentHistory } from "./Student/DashboardComponent/StudentPaymentHistory";
-// import { StudentStudyMaterial } from "./Student/DashboardComponent/StudentStudyMaterial";
-// import { StudentHomeWorks } from "./Student/DashboardComponent/StudentHomeworks";
-// import StudentEvents from "./Student/DashboardComponent/StudentEvents";
-// import { StudentTimeTable } from "./Student/DashboardComponent/StudentTimetable";
-
-// const TestYk = () => {
-//   return (
-//     <>
-//       {/* <StudentDetails /> */}
-//       {/* <StudentFeeInvoicesComponent /> */}
-//       {/* <StudentFeeStructure /> */}
-//       {/* <StudentPaymentHistory /> */}
-//       {/* <StudentStudyMaterial /> */}
-//       {/* <StudentHomeWorks /> */}
-//       {/* <StudentEvents /> */}
-      
-
-//     </>
-//   );
-// };
-// export default TestYk;
+const TestYk = () => {
+  return (
+    <>
+      <LibraryBooksAddModal />
+    </>
+  );
+};
+export default TestYk;
--- client/src/Routes/AppRoutes.jsx
+++ client/src/Routes/AppRoutes.jsx
@@ -8,15 +8,16 @@ import AdminStudentPage from "../Pages/Users/Admin/AdminStudentPage";
 import Allstudents from "../Pages/Users/Admin/Allstudents";
 // import TestYk from "../Components/TestYk";
 import LandingPage from "../Pages/LandingPage/landingPage";
-import Login from '../Pages/Login/loginPage'
+import Login from "../Pages/Login/loginPage";
 import StudentDashBoard from "../Pages/Student/Dashboard/StudentDashBoard";
 import StudentFeeInvoice from "../Pages/Student/Dashboard/StudentFeeInvoice";
 import StudentFeeStructure from "../Pages/Student/Dashboard/StudentFeeStructure";
-import StudentHomeWorks from '../Pages/Student/Dashboard/StudentsHomeWorks'
+import StudentHomeWorks from "../Pages/Student/Dashboard/StudentsHomeWorks";
 import PaymentHistory from "../Pages/Student/Dashboard/StudentPaymentHistory";
 import StudyMaterials from "../Pages/Student/Dashboard/StudyMaterials";
 import StaffDashBoard from "../Pages/Staff/Dashboard/StaffDashBoard";
 import LibraryManagemnet from "../Pages/Staff/Dashboard/Library";
+import TestYk from "../Components/TestYk";
 const AppRoutes = () => {
   const { loading } = useSelector((state) => state.alerts);
 
@@ -35,25 +36,30 @@ const AppRoutes = () => {
         {/* STUDENTS ROUTS */}
         <Route path={RouteObjects.root} element={<LandingPage />} />
         <Route path={RouteObjects.Login} element={<Login />} />
-        <Route path={RouteObjects.StudentDashboard} element={<StudentDashBoard/>} />
-        <Route path={RouteObjects.FeeInvoice} element={<StudentFeeInvoice/>} />
-        <Route path={RouteObjects.FeeStructure} element={<StudentFeeStructure/>} />
-        <Route path={RouteObjects.HomeWorks} element={<StudentHomeWorks/>} />
-        <Route path={RouteObjects.Payment} element={<PaymentHistory/>} />
-        <Route path={RouteObjects.StudyMaterials} element={<StudyMaterials/>} />
+        <Route
+          path={RouteObjects.StudentDashboard}
+          element={<StudentDashBoard />}
+        />
+        <Route path={RouteObjects.FeeInvoice} element={<StudentFeeInvoice />} />
+        <Route
+          path={RouteObjects.FeeStructure}
+          element={<StudentFeeStructure />}
+        />
+        <Route path={RouteObjects.HomeWorks} element={<StudentHomeWorks />} />
+        <Route path={RouteObjects.Payment} element={<PaymentHistory />} />
+        <Route
+          path={RouteObjects.StudyMaterials}
+          element={<StudyMaterials />}
+        />
 
-
-
-        <Route path={RouteObjects.StaffDashboard} element={<StaffDashBoard/>} />
-        <Route path={RouteObjects.Stafflibrary} element={<LibraryManagemnet/>} />
-
-
-
-
-
-
-
-
+        <Route
+          path={RouteObjects.StaffDashboard}
+          element={<StaffDashBoard />}
+        />
+        <Route
+          path={RouteObjects.Stafflibrary}
+          element={<LibraryManagemnet />}
+        />
 
         <Route
           path={RouteObjects.AdminStudentPage}
@@ -61,7 +67,7 @@ const AppRoutes = () => {
         />
         <Route path={RouteObjects.AllstudentsPage} element={<Allstudents />} />
         <Route path="/test" element={<Test />} />
-        {/* <Route path="/testyk" element={<TestYk />} /> */}
+        <Route path="/testyk" element={<TestYk />} />
       </Routes>
     </div>
   );
