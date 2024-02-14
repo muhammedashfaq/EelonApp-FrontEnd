a7deb51b3fe6fc93e48293055973d0610ca2b6d6
Yadu Krishna <yaduk946@gmail.com>
2024-02-14 22:43:39 +0530

    added reports routes in library settings

--- client/src/Components/Charts/Library/LibraryPieReportChart.jsx
+++ client/src/Components/Charts/Library/LibraryPieReportChart.jsx
@@ -1,55 +1,80 @@
-import React from 'react';
-import ReactApexChart from 'react-apexcharts';
+import React from "react";
+import ReactApexChart from "react-apexcharts";
+import { useState, useEffect } from "react";
+import useAxiosPrivate from "../../../Hooks/useAxiosPrivate";
 
 const LibraryPieReportChart = () => {
-    const options = {
-        chart: {
-          width: 380,
-          type: 'donut',
-        },
-        plotOptions: {
-          pie: {
-            startAngle: -90,
-            endAngle: 270,
-          },
-        },
-        dataLabels: {
-          enabled: false,
-        },
-        fill: {
-          type: 'gradient',
-        },
-        legend: {
-          formatter: (val, opts) => val + " - " + opts.w.globals.series[opts.seriesIndex],
-        },
-        title: {
-          text: 'Gradient Donut with custom Start-angle',
-        },
-        responsive: [
-          {
-            breakpoint: 480,
-            options: {
-              chart: {
-                width: 200,
-              },
-              legend: {
-                position: 'bottom',
-              },
-            },
-          },
-        ],
-      };
-    
-      const series = [44, 55, 41, 17, 15];
-    
-      return (
-        <div>
-          <div id="chart">
-            <ReactApexChart options={options} series={series} type="donut" width={380} />
-          </div>
-          <div id="html-dist"></div>
-        </div>
-      );
-    };
+  const [genreCount, setgenreCount] = useState();
 
-export default LibraryPieReportChart
+  const axiosPrivate = useAxiosPrivate();
+
+  const getGenreCount = async () => {
+    try {
+      const response = await axiosPrivate.get("library/reports/genrecount");
+      setgenreCount(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getGenreCount();
+  }, []);
+
+  const options = {
+    chart: {
+      width: 380,
+      type: "donut",
+    },
+    plotOptions: {
+      pie: {
+        startAngle: -90,
+        endAngle: 270,
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    fill: {
+      type: "gradient",
+    },
+    legend: {
+      formatter: (val, opts) =>
+        val + " - " + opts.w.globals.series[opts.seriesIndex],
+    },
+    title: {
+      text: "Gradient Donut with custom Start-angle",
+    },
+    responsive: [
+      {
+        breakpoint: 480,
+        options: {
+          chart: {
+            width: 200,
+          },
+          legend: {
+            position: "bottom",
+          },
+        },
+      },
+    ],
+  };
+
+  const series = [44, 55, 41, 17, 15];
+
+  return (
+    <div>
+      <div id="chart">
+        <ReactApexChart
+          options={options}
+          series={series}
+          type="donut"
+          width={380}
+        />
+      </div>
+      <div id="html-dist"></div>
+    </div>
+  );
+};
+
+export default LibraryPieReportChart;
