6301e7827568c8210dc6b77742e0180e19322488
Yadu Krishna <yaduk946@gmail.com>
2024-02-19 07:39:13 +0530

    Added day to exam table row

--- client/src/Components/Staff/DashboardComponents/Timetables/ExamTimeTable/ExamTableRow.jsx
+++ client/src/Components/Staff/DashboardComponents/Timetables/ExamTimeTable/ExamTableRow.jsx
@@ -1,5 +1,11 @@
 import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
-import { IconButton, Input, Option, Select } from "@material-tailwind/react";
+import {
+  IconButton,
+  Input,
+  Option,
+  Select,
+  Typography,
+} from "@material-tailwind/react";
 import React, { useEffect } from "react";
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -11,10 +17,12 @@ const ExamTableRow = ({ index, removeDiv, handleData, uuid }) => {
   const [FNPortions, setFNPortions] = useState();
   const [ANSub, setANSub] = useState();
   const [ANPortions, setANPortions] = useState();
+  const [day, setday] = useState();
 
   const createData = () => {
     const jsonData = {
       id: uuid,
+      day,
       date,
       FnSub,
       FNPortions,
@@ -25,18 +33,29 @@ const ExamTableRow = ({ index, removeDiv, handleData, uuid }) => {
   };
   useEffect(() => {
     createData();
-  }, [date, FnSub, FNPortions, ANSub, ANPortions]);
+  }, [date, FnSub, FNPortions, ANSub, ANPortions, day]);
+
+  useEffect(() => {
+    if (!date) return;
+    const newDate = new Date(date);
+    setday(newDate.toLocaleDateString("en-In", { weekday: "long" }));
+  }, [date]);
 
   return (
     <>
       <div className="h-16 w-auto bg-gray-200 my-5 px-10 shadow-2xl rounded-2xl shadow-black flex justify-between items-center">
         <div>{index}</div>
-        <div>
+        <div className="flex gap-5 items-center">
           <Input
             type="date"
             value={date}
             onChange={(e) => setdate(e.target.value)}
           />
+          {day && (
+            <Typography variant="h6" color="gray">
+              {day}
+            </Typography>
+          )}
         </div>
         <div className="w-72 flex gap-2">
           <Input
