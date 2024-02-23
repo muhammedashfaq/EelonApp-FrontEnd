0313cfeb9761164f89f27a2eb57c836f0888ff34
muhammedashfaq <123805225+muhammedashfaq@users.noreply.github.com>
2024-02-23 09:17:02 +0530

    someUI

--- client/src/Components/Admin/DashoardComponents/MainDashBoard.jsx
+++ client/src/Components/Admin/DashoardComponents/MainDashBoard.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Chart from './Chart'
 
 const MainDashBoard = () => {
   return (
@@ -10,18 +11,18 @@ const MainDashBoard = () => {
                    <div className="w-full grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-4">
                       <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8  2xl:col-span-2">
                          <div className="flex items-center justify-between mb-4">
-                            <div className="flex-shrink-0">
+                            {/* <div className="flex-shrink-0">
                                <span className="text-2xl sm:text-3xl leading-none font-bold text-gray-900">$45,385</span>
                                <h3 className="text-base font-normal text-gray-500">Sales this week</h3>
-                            </div>
-                            <div className="flex items-center justify-end flex-1 text-green-500 text-base font-bold">
+                            </div> */}
+                            {/* <div className="flex items-center justify-end flex-1 text-green-500 text-base font-bold">
                                12.5%
                                <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                   <path fill-rule="evenodd" d="M5.293 7.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L6.707 7.707a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
                                </svg>
-                            </div>
+                            </div> */}
                          </div>
-                         <div id="main-chart"></div>
+                         <div id="main-chart"> <Chart/>  </div>
                       </div>
                       <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8 ">
                          <div className="mb-4 flex items-center justify-between">
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,29 +1,29 @@
-import withMT from "@material-tailwind/react/utils/withMT";
+  import withMT from "@material-tailwind/react/utils/withMT";
 
-import flowbite from "flowbite/plugin"
+  import flowbite from "flowbite/plugin"
 
-export default withMT({
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  theme: {
-    screens: {
-      'mobile': '100px',
-      'ipad': '768px',
-      'Tablet': '1024px',
-      'Laptop': '1280px',
-    },
-
-    extend: {
-      colors: {
-        "dark-purple": "#081A51",
-        "light-white": "rgba(255,255,255,0.18)",
+  export default withMT({
+    content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+    theme: {
+      screens: {
+        'mobile': '100px',
+        'ipad': '768px',
+        'Tablet': '1024px',
+        'Laptop': '1280px',
       },
-      backgroundImage: {
-        'student': "url('./src/assets/student.svg')",
-        'teacher': "url('./src/assets/teacher-selected.svg')",
-      },
-      
-    },
-  },
-  plugins: [flowbite]
 
-});
+      extend: {
+        colors: {
+          "dark-purple": "#081A51",
+          "light-white": "rgba(255,255,255,0.18)",
+        },
+        backgroundImage: {
+          'student': "url('./src/assets/student.svg')",
+          'teacher': "url('./src/assets/teacher-selected.svg')",
+        },
+        
+      },
+    },
+    plugins: [flowbite]
+
+  });
