5594c9dc362842d0ff102c6dcae63b01d100c907
Yadu Krishna <yaduk946@gmail.com>
2024-03-01 08:27:57 +0530

    added image cropping staff profile page

--- client/src/Components/Staff/DashboardComponents/StaffList/StaffProfile.jsx
+++ client/src/Components/Staff/DashboardComponents/StaffList/StaffProfile.jsx
@@ -13,40 +13,31 @@ import { Button, Tooltip } from "@material-tailwind/react";
 import notFoundImg from "../../../../assets/placeholderImg.jpg";
 
 import React, { useState } from "react";
-import useAxiosPrivate from "../../../../Hooks/useAxiosPrivate";
 import { faSquarePlus } from "@fortawesome/free-regular-svg-icons";
 import AddStaffExperianceDocuModal from "./AddStaffExperianceDocuModal";
 import AddStaffEducationalModal from "./AddStaffEducationalModal";
+import CropImageModal from "../../../Admin/CropImageModal";
+import StaffUploadImgModal from "./StaffUploadImgModal";
 
-const StaffProfile = ({ userData }) => {
-  const axiosPrivate = useAxiosPrivate();
-
+const StaffProfile = ({ userData, getData }) => {
   const [image, setImage] = useState();
 
-  const submitImage = async () => {
-    try {
-      const response = await axiosPrivate.post("");
-    } catch (error) {
-      console.log(error);
-    }
-  };
   return (
     <div class="container mx-auto my-5 p-5">
-    <div class="md:flex no-wrap md:-mx-2 ">
+      <div class="md:flex no-wrap md:-mx-2 ">
         <div class="w-full md:w-3/12 md:mx-2">
-
-
-
-
-
           <div className="image overflow-hidden">
             <img
               className="h-auto w-full mx-auto"
-              src={userData?.userData?.url || image || notFoundImg}
+              src={userData?.profilePic?.url || image || notFoundImg}
               alt=""
+              style={{ borderRadius: "10px" }}
             />
           </div>
-          <div className=" flex mobile:flex-wrap Tablet:flex-wrap ipad:flex-wrap gap-2 justify-end my-1 items-center">
+          <div className="m-3">
+            <StaffUploadImgModal userId={userData?._id} getData={getData} />
+          </div>
+          {/* <div className=" flex mobile:flex-wrap Tablet:flex-wrap ipad:flex-wrap gap-2 justify-end my-1 items-center">
             <Button color="teal">
               <label
                 htmlFor="imageUploadfather"
@@ -66,7 +57,6 @@ const StaffProfile = ({ userData }) => {
               <FontAwesomeIcon
                 icon={faUpload}
                 size="1x"
-                onClick={submitImage}
               />
             </Button>
             <Button
@@ -76,7 +66,7 @@ const StaffProfile = ({ userData }) => {
             >
               <FontAwesomeIcon icon={faTrash} size="1x" />
             </Button>
-          </div>
+          </div> */}
           <h1 className="text-gray-900 font-bold text-xl leading-8 my-1">
             {userData?.name}
           </h1>
@@ -98,127 +88,150 @@ const StaffProfile = ({ userData }) => {
               <span className="ml-auto">{userData?.DOJ}</span>
             </li>
           </ul>
+        </div>
 
+        <div className="w-full md:w-9/12 mx-2 h-64">
+          <div className="bg-white p-3 shadow-sm rounded-sm">
+            <div className="flex items-center space-x-2 font-semibold text-gray-900 leading-8">
+              <FontAwesomeIcon icon={faUser} />
+              <span className="tracking-wide">About</span>
+            </div>
+            <div className="text-gray-700">
+              <div className="grid md:grid-cols-2 text-sm">
+                <div className="grid grid-cols-2">
+                  <div className="px-4 py-2 font-semibold">Full Name</div>
+                  <div className="px-4 py-2">{userData?.name}</div>
+                </div>
 
+                <div className="grid grid-cols-2">
+                  <div className="px-4 py-2 font-semibold">Gender</div>
+                  <div className="px-4 py-2">{userData?.gender}</div>
+                </div>
+                <div className="grid grid-cols-2">
+                  <div className="px-4 py-2 font-semibold">Contact No.</div>
+                  <div className="px-4 py-2">
+                    {userData?.mob} <span className="font-bold">&</span>{" "}
+                    {userData?.mob}
+                  </div>
+                </div>
 
-      </div>
-
-      <div className="w-full md:w-9/12 mx-2 h-64">
-        <div className="bg-white p-3 shadow-sm rounded-sm">
-          <div className="flex items-center space-x-2 font-semibold text-gray-900 leading-8">
-            <FontAwesomeIcon icon={faUser} />
-            <span className="tracking-wide">About</span>
+                <div className="grid grid-cols-2">
+                  <div className="px-4 py-2 font-semibold">
+                    Permanant Address
+                  </div>
+                  <div className="px-4 py-2">{userData?.address}</div>
+                </div>
+                <div className="grid grid-cols-2">
+                  <div className="px-4 py-2 font-semibold">Email.</div>
+                  <div className="px-4 py-2">
+                    <a className="text-blue-800" href="mailto:jane@example.com">
+                      {userData?.email}
+                    </a>
+                  </div>
+                </div>
+                <div className="grid grid-cols-2">
+                  <div className="px-4 py-2 font-semibold">Birthday</div>
+                  <div className="px-4 py-2">{userData?.DOB}</div>
+                </div>
+                <div className="grid grid-cols-2">
+                  <div className="px-4 py-2 font-semibold">Nationality</div>
+                  <div className="px-4 py-2">{userData?.nationality}</div>
+                </div>
+                <div className="grid grid-cols-2">
+                  <div className="px-4 py-2 font-semibold">State</div>
+                  <div className="px-4 py-2">{userData?.state}</div>
+                </div>
+                <div className="grid grid-cols-2">
+                  <div className="px-4 py-2 font-semibold">City</div>
+                  <div className="px-4 py-2">{userData?.city}</div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="text-gray-700">
-            <div className="grid md:grid-cols-2 text-sm">
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold">Full Name</div>
-                <div className="px-4 py-2">{userData?.name}</div>
-              </div>
 
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold">Gender</div>
-                <div className="px-4 py-2">{userData?.gender}</div>
-              </div>
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold">Contact No.</div>
-                <div className="px-4 py-2">
-                  {userData?.mob} <span className="font-bold">&</span>{" "}
-                  {userData?.mob}
-                </div>
-              </div>
+          <div className="my-4"></div>
 
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold">Permanant Address</div>
-                <div className="px-4 py-2">{userData?.address}</div>
-              </div>
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold">Email.</div>
-                <div className="px-4 py-2">
-                  <a className="text-blue-800" href="mailto:jane@example.com">
-                    {userData?.email}
-                  </a>
+          <div className="bg-white p-3 shadow-sm rounded-sm">
+            <div className="grid grid-cols-2">
+              <div>
+                <div className="flex items-center space-x-2 font-semibold text-gray-900 leading-8 mb-4">
+                  <FontAwesomeIcon icon={faBook} />
+                  <div className="flex space-x-5 w-full">
+                    <span className="tracking-wide">Experience</span>
+                    <AddStaffExperianceDocuModal />
+                  </div>
                 </div>
+
+                <ul className="list-inside space-y-2">
+                  <li>
+                    <div className="text-teal-600">
+                      Owner at Her Company Inc.
+                    </div>
+                    <div className="text-gray-500 text-xs">
+                      March 2020 - Now
+                    </div>
+                  </li>
+                  <li>
+                    <div className="text-teal-600">
+                      Owner at Her Company Inc.
+                    </div>
+                    <div className="text-gray-500 text-xs">
+                      March 2020 - Now
+                    </div>
+                  </li>
+                  <li>
+                    <div className="text-teal-600">
+                      Owner at Her Company Inc.
+                    </div>
+                    <div className="text-gray-500 text-xs">
+                      March 2020 - Now
+                    </div>
+                  </li>
+                  <li>
+                    <div className="text-teal-600">
+                      Owner at Her Company Inc.
+                    </div>
+                    <div className="text-gray-500 text-xs">
+                      March 2020 - Now
+                    </div>
+                  </li>
+                </ul>
               </div>
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold">Birthday</div>
-                <div className="px-4 py-2">{userData?.DOB}</div>
-              </div>
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold">Nationality</div>
-                <div className="px-4 py-2">{userData?.nationality}</div>
-              </div>
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold">State</div>
-                <div className="px-4 py-2">{userData?.state}</div>
-              </div>
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold">City</div>
-                <div className="px-4 py-2">{userData?.city}</div>
+              <div>
+                <div className="flex items-center space-x-2 font-semibold text-gray-900 leading-8 mb-4">
+                  <span>
+                    <FontAwesomeIcon icon={faGraduationCap} />
+                  </span>
+
+                  <div className="flex space-x-5 w-full">
+                    <span className="tracking-wide">Education</span>
+
+                    <AddStaffEducationalModal />
+                  </div>
+                </div>
+                <ul className="list-inside space-y-2">
+                  <li>
+                    <div className="text-teal-600">
+                      Masters Degree in Oxford
+                    </div>
+                    <div className="text-gray-500 text-xs">
+                      March 2020 - Now
+                    </div>
+                  </li>
+                  <li>
+                    <div className="text-teal-600">
+                      Bachelors Degreen in LPU
+                    </div>
+                    <div className="text-gray-500 text-xs">
+                      March 2020 - Now
+                    </div>
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
         </div>
-
-        <div className="my-4"></div>
-
-        <div className="bg-white p-3 shadow-sm rounded-sm">
-          <div className="grid grid-cols-2">
-            <div>
-              <div className="flex items-center space-x-2 font-semibold text-gray-900 leading-8 mb-4">
-                <FontAwesomeIcon icon={faBook} />
-                <div className="flex space-x-5 w-full">
-                  <span className="tracking-wide">Experience</span>
-                  <AddStaffExperianceDocuModal />
-                </div>
-              </div>
-
-              <ul className="list-inside space-y-2">
-                <li>
-                  <div className="text-teal-600">Owner at Her Company Inc.</div>
-                  <div className="text-gray-500 text-xs">March 2020 - Now</div>
-                </li>
-                <li>
-                  <div className="text-teal-600">Owner at Her Company Inc.</div>
-                  <div className="text-gray-500 text-xs">March 2020 - Now</div>
-                </li>
-                <li>
-                  <div className="text-teal-600">Owner at Her Company Inc.</div>
-                  <div className="text-gray-500 text-xs">March 2020 - Now</div>
-                </li>
-                <li>
-                  <div className="text-teal-600">Owner at Her Company Inc.</div>
-                  <div className="text-gray-500 text-xs">March 2020 - Now</div>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <div className="flex items-center space-x-2 font-semibold text-gray-900 leading-8 mb-4">
-                <span>
-                  <FontAwesomeIcon icon={faGraduationCap} />
-                </span>
-
-                <div className="flex space-x-5 w-full">
-                  <span className="tracking-wide">Education</span>
-
-                  <AddStaffEducationalModal />
-                </div>
-              </div>
-              <ul className="list-inside space-y-2">
-                <li>
-                  <div className="text-teal-600">Masters Degree in Oxford</div>
-                  <div className="text-gray-500 text-xs">March 2020 - Now</div>
-                </li>
-                <li>
-                  <div className="text-teal-600">Bachelors Degreen in LPU</div>
-                  <div className="text-gray-500 text-xs">March 2020 - Now</div>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
       </div>
-    </div>
     </div>
   );
 };
---
+++ client/src/Components/Staff/DashboardComponents/StaffList/StaffUploadImgModal.jsx
@@ -0,0 +1,123 @@
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  Dialog,
+  DialogHeader,
+  DialogBody,
+  DialogFooter,
+  IconButton,
+} from "@material-tailwind/react";
+import CropImageModal from "../../../Admin/CropImageModal";
+import { FileUploader } from "react-drag-drop-files";
+import useAxiosPrivate from "../../../../Hooks/useAxiosPrivate";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faTrash, faUpload } from "@fortawesome/free-solid-svg-icons";
+
+const fileTypes = ["JPG", "PNG", "GIF", "JPEG"];
+
+export default function StaffUploadImgModal({ userId, getData }) {
+  const [open, setOpen] = useState(false);
+  const [file, setFile] = useState(null);
+  const [editImg, setEditImg] = useState(null);
+  const [openModal, setOpenModal] = useState(false);
+
+  const axiosPrivate = useAxiosPrivate();
+
+  const uploadImage = async () => {
+    if (!userId) return;
+    try {
+      const response = await axiosPrivate.post(
+        `images/staff/profilepic/${userId}`,
+        {
+          Image: editImg,
+        }
+      );
+      console.log(response);
+      setOpen(false);
+      getData();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleOpen = () => setOpen(!open);
+
+  const handleFileChange = (file) => {
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      setFile(reader.result);
+    };
+    reader.onerror = (error) => {
+      console.error("Error :", error);
+    };
+    setOpenModal(true);
+    // setFile(file);
+  };
+
+  useEffect(() => {
+    if (!file) return;
+    setOpenModal(true);
+  }, [file]);
+
+  //   useEffect(() => {
+  //     console.log(editImg);
+  //   }, [editImg]);
+
+  return (
+    <>
+      <IconButton onClick={handleOpen} variant="outlined">
+        <FontAwesomeIcon icon={faEdit} />
+      </IconButton>
+      <Dialog size="sm" open={open} handler={handleOpen}>
+        <DialogHeader>Upload image</DialogHeader>
+        <DialogBody>
+          <div className="flex justify-center">
+            {file ? (
+              <img src={editImg} width="300px" />
+            ) : (
+              <FileUploader
+                handleChange={handleFileChange}
+                types={fileTypes}
+                name="staffImage"
+                hoverTitle="Drop image here"
+              />
+            )}
+            <CropImageModal
+              editImg={editImg}
+              setEditImg={setEditImg}
+              file={file}
+              setFile={setFile}
+              openModal={openModal}
+              setOpenModal={setOpenModal}
+            />
+          </div>
+        </DialogBody>
+        <DialogFooter>
+          <Button
+            variant="text"
+            color="red"
+            onClick={() => {
+              setOpen(false);
+              setFile(null);
+            }}
+            className="mr-1"
+          >
+            <span>Cancel</span>
+          </Button>
+          <Button variant="gradient" color="green" onClick={uploadImage}>
+            <span>Upload</span>
+          </Button>
+          {/* <div className=" flex mobile:flex-wrap Tablet:flex-wrap ipad:flex-wrap gap-2 justify-end my-1 items-center">
+            <Button color="lime">
+              <FontAwesomeIcon icon={faUpload} size="1x" />
+            </Button>
+            <Button color="red" className="">
+              <FontAwesomeIcon icon={faTrash} size="1x" />
+            </Button>
+          </div> */}
+        </DialogFooter>
+      </Dialog>
+    </>
+  );
+}
