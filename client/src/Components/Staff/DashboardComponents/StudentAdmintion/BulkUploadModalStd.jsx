98ea825c05543ef1219373d428fbcd36b674bcc9
Yadu Krishna <yaduk946@gmail.com>
2024-03-04 23:50:31 +0530

    fix problems with student deletion

--- client/src/Components/Staff/DashboardComponents/StudentAdmintion/BulkUploadModalStd.jsx
+++ client/src/Components/Staff/DashboardComponents/StudentAdmintion/BulkUploadModalStd.jsx
@@ -10,54 +10,76 @@ import {
   Typography,
 } from "@material-tailwind/react";
 import { axiosFormdata } from "../../../../api/axios";
- 
+
 const BulkUploadModalStd = () => {
-    const [xlsFile,setXlsFile]=useState("")
-    const [isValidate,setIsValidate]=useState(false)
-    const [isLoading ,setisLoading]=useState(false)
-    const [open, setOpen] = React.useState(false);
-  
+  const [xlsFile, setXlsFile] = useState("");
+  const [isValidate, setIsValidate] = useState(false);
+  const [isLoading, setisLoading] = useState(false);
+  const [open, setOpen] = React.useState(false);
+
   const handleOpen = () => setOpen(!open);
 
-  const handleSubmit = async()=>{
+  const handleSubmit = async () => {
     try {
-        setisLoading(true)
-        const response = await axiosFormdata.post("",xlsFile)
-        setisLoading(false)
+      if (!xlsFile) return;
+      setisLoading(true);
+      const formData = new FormData();
+      formData.append("file", xlsFile);
+      const response = await axiosFormdata.post(
+        "bulkuploads/student",
+        formData
+      );
+      console.log(response);
+      setisLoading(false);
     } catch (error) {
-        setisLoading(false)
-        console.log(error);
+      setisLoading(false);
+      console.log(error);
     }
-  }
+  };
 
-  useEffect(()=>{
-const isValidate = xlsFile
-setIsValidate(isValidate)
-  },[xlsFile])
+  useEffect(() => {
+    const isValidate = xlsFile;
+    setIsValidate(isValidate);
+  }, [xlsFile]);
   return (
     <>
-    <span className="text-white font-normal cursor-pointer" onClick={handleOpen}>Bulk Upload</span>
-  <Dialog open={open} size="xs" >
+      <span
+        className="text-white font-normal cursor-pointer"
+        onClick={handleOpen}
+      >
+        Bulk Upload
+      </span>
+      <Dialog open={open} size="xs">
+        <DialogBody>
+          <div className="grid gap-6">
+            <Typography className="-mb-1" color="blue-gray" variant="h6">
+              Username
+            </Typography>
+            <Input
+              accept=".xlsx"
+              type="file"
+              label="Username"
+              onChange={(e) => setXlsFile(e.target.files[0])}
+            />
+          </div>
+        </DialogBody>
+        <DialogFooter className="space-x-2">
+          <Button variant="text" color="gray" onClick={handleOpen}>
+            cancel
+          </Button>
+          <Button
+            loading={isLoading}
+            disabled={!isValidate}
+            variant="gradient"
+            color="gray"
+            onClick={handleSubmit}
+          >
+            UpLoad
+          </Button>
+        </DialogFooter>
+      </Dialog>
+    </>
+  );
+};
 
-      <DialogBody>
-    
-        <div className="grid gap-6">
-          <Typography className="-mb-1" color="blue-gray" variant="h6">
-            Username
-          </Typography>
-          <Input accept=".xlsx" type="file" label="Username" onChange={(e)=>setXlsFile(e.target.value)} />
-        </div>
-      </DialogBody>
-      <DialogFooter className="space-x-2">
-        <Button variant="text" color="gray" onClick={handleOpen}>
-          cancel
-        </Button>
-        <Button loading={isLoading} disabled={!isValidate} variant="gradient" color="gray" onClick={handleSubmit}>
-          UpLoad
-        </Button>
-      </DialogFooter>
-    </Dialog>
-  </>  )
-}
-
-export default BulkUploadModalStd
+export default BulkUploadModalStd;
