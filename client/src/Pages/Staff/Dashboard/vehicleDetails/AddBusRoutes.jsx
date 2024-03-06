9f6dd3fe81a1e0aedd03ddcccec162107da9ee1f
Yadu Krishna <yaduk946@gmail.com>
2024-03-06 13:23:33 +0530

    Added draggable bus stop row

--- client/src/Pages/Staff/Dashboard/vehicleDetails/AddBusRoutes.jsx
+++ client/src/Pages/Staff/Dashboard/vehicleDetails/AddBusRoutes.jsx
@@ -1,13 +1,34 @@
 import { useEffect, useState } from "react";
 import Banner from "../../../../Components/Banner/Banner";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import { IconButton } from "@material-tailwind/react";
+import { faPlus, faSearch } from "@fortawesome/free-solid-svg-icons";
+import {
+  IconButton,
+  Input,
+  Option,
+  Select,
+  Typography,
+} from "@material-tailwind/react";
 import BusRouteRow from "./BusRouteRow";
 import { v4 as uuidv4 } from "uuid";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import useAxiosPrivate from "../../../../Hooks/useAxiosPrivate";
 
 const AddBusRoutes = () => {
   const [dataArray, setDataArray] = useState([]);
+  const [busNo, setbusNo] = useState();
+  const [busDDs, setBusDDs] = useState([]);
+  const axiosPrivate = useAxiosPrivate();
+
+  const getDropdownData = async () => {
+    try {
+      const response = await axiosPrivate.get("transportation/bus/dropdowns");
+      setBusDDs(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const addDiv = () => {
     const newId = uuidv4();
@@ -35,35 +56,92 @@ const AddBusRoutes = () => {
     }
   };
 
+  const onDragEnd = (result) => {
+    if (!result.destination) {
+      return;
+    }
+
+    const items = Array.from(dataArray);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    setDataArray(items);
+  };
+
   useEffect(() => {
     console.log(dataArray);
   }, [dataArray]);
 
+  useEffect(() => {
+    getDropdownData();
+  }, []);
   return (
     <>
       <Banner />
       <div className="px-5">
         <div
-          className="h-14 bg-blue-gray-300 w-full shadow-2xl mt-10 flex flex-row-reverse items-center px-5"
+          className="h-20 bg-gray-600 w-full shadow-2xl mt-10 flex justify-between items-center px-5"
           style={{ borderRadius: "15px" }}
         >
-          <div></div>
+          <div className="flex items-center gap-3">
+            <Typography variant="h6">Bus no:</Typography>
+            <Select label="Select bus no">
+              {busDDs && busDDs.map((item) => <Option>{item.busNo}</Option>)}
+            </Select>
+
+            {/* <Typography variant="h6">Enter bus no.</Typography>
+            <form>
+              <Input
+                label="Bus number"
+                onChange={(e) => setbusNo(e.target.value)}
+              />
+            </form>
+            <IconButton variant="outlined">
+              <FontAwesomeIcon icon={faSearch} />
+            </IconButton> */}
+          </div>
           <IconButton variant="outlined" size="sm" onClick={addDiv}>
             <FontAwesomeIcon icon={faPlus} size="xl" />
           </IconButton>
         </div>
       </div>
       <div>
-        {dataArray &&
-          dataArray.map((item, index) => (
-            <BusRouteRow
-              key={item?.id}
-              item={item}
-              handleData={handleData}
-              removeDiv={removeDiv}
-              index={index + 1}
-            />
-          ))}
+        <div className="flex justify-center">
+          <div style={{ width: "500px" }}>
+            <DragDropContext onDragEnd={onDragEnd}>
+              <Droppable droppableId="droppable">
+                {(provided) => (
+                  <div ref={provided.innerRef} {...provided.droppableProps}>
+                    {dataArray.map((item, index) => (
+                      <Draggable
+                        key={item?.id}
+                        draggableId={item?.id}
+                        index={index}
+                      >
+                        {(provided) => (
+                          <div
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                          >
+                            <BusRouteRow
+                              key={item?.id}
+                              item={item}
+                              handleData={handleData}
+                              removeDiv={removeDiv}
+                              index={index + 1}
+                            />
+                          </div>
+                        )}
+                      </Draggable>
+                    ))}
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+            </DragDropContext>
+          </div>
+        </div>
       </div>
     </>
   );
--- client/src/Pages/Staff/Dashboard/vehicleDetails/BusRouteRow.jsx
+++ client/src/Pages/Staff/Dashboard/vehicleDetails/BusRouteRow.jsx
@@ -7,9 +7,22 @@ import {
   Typography,
   Option,
 } from "@material-tailwind/react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const BusRouteRow = ({ removeDiv, handleData, item, index }) => {
+  const [StopName, setStopName] = useState();
+
+  const createData = () => {
+    const jsonData = {
+      id: item.id,
+      index,
+      stopName: StopName,
+    };
+    handleData(jsonData);
+  };
+  useEffect(() => {
+    createData();
+  }, [StopName, index]);
   return (
     <>
       <div className="h-28 w-auto bg-gray-200 m-5 px-10 shadow-2xl rounded-2xl shadow-black flex justify-between items-center">
@@ -20,19 +33,11 @@ const BusRouteRow = ({ removeDiv, handleData, item, index }) => {
           {index}
         </div>
         <div className="flex flex-col">
-          <Typography variant="h6">Bus no.</Typography>
-          <Select>
-            <Option>bus no</Option>
-            <Option>bus no</Option>
-          </Select>
-        </div>
-        <div className="flex flex-col">
-          <Typography variant="h6">From</Typography>
-          <Input label="From" />
-        </div>
-        <div className="flex flex-col">
-          <Typography variant="h6">Destination</Typography>
-          <Input label="Destination" />
+          <Typography variant="h6">Stop name</Typography>
+          <Input
+            label="Destination"
+            onChange={(e) => setStopName(e.target.value)}
+          />
         </div>
         <IconButton variant="gradient" onClick={() => removeDiv(item.id)}>
           <FontAwesomeIcon icon={faMinus} />
