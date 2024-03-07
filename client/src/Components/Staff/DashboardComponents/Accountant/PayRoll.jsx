2f6803fc71241ffc4e8499d25d8e210074b1ae24
Yadu Krishna <yaduk946@gmail.com>
2024-03-07 16:37:50 +0530

    fixed uncontrolled request in study room

--- client/src/Components/Staff/DashboardComponents/Accountant/PayRoll.jsx
+++ client/src/Components/Staff/DashboardComponents/Accountant/PayRoll.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import {Button, Input, Select, Typography, Option} from '@material-tailwind/react';
+import useAxiosPrivate from '../../../../Hooks/useAxiosPrivate';
 
 const PayRoll = () => {
-  return (
-    <div>PayRoll</div>
-  )
-}
+  const axiosPrivate = useAxiosPrivate();
+  const [searchType, setsearchType] = useState('staffName');
+  const [searchQuery, setsearchQuery] = useState();
 
-export default PayRoll
+  const filterStaff = async (req, res) => {
+    try {
+      let reqData = {};
+      if (searchType === 'staffName') {
+        reqData = {staffName: searchQuery};
+      } else {
+        reqData = {s};
+      }
+
+      const response = await axiosPrivate.put('payroll/staff/filter');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <>
+      <div className='flex justify-center'>
+        <div className='container xl mt-12'>
+          <div className='flex gap-3 items-baseline  p-5 rounded-2xl shadow-2xl bg-gradient-to-r bg-blue-gray-800 justify-center'>
+            {/* <Typography variant='h6'>Search staff</Typography> */}
+            <div className='w-52'>
+              <Select label='Select'>
+                <Option>Staff name</Option>
+                <Option>Staff id</Option>
+              </Select>
+            </div>
+            <div className='w-60'>
+              <Input label='Search staff' />
+            </div>
+            <Button color='teal' variant='contained'>
+              Search
+            </Button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PayRoll;
