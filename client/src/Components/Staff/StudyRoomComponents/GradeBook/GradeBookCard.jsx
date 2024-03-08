6cf18dd42a2a89a1624e2f39fb3625ef2e3b0007
Yadu Krishna <yaduk946@gmail.com>
2024-03-08 17:42:16 +0530

    Added gradeBooks

--- client/src/Components/Staff/StudyRoomComponents/GradeBook/GradeBookCard.jsx
+++ client/src/Components/Staff/StudyRoomComponents/GradeBook/GradeBookCard.jsx
@@ -1,10 +1,28 @@
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faBook} from '@fortawesome/free-solid-svg-icons';
+import {faBook, faEllipsisV} from '@fortawesome/free-solid-svg-icons';
+import useAxiosPrivate from '../../../../Hooks/useAxiosPrivate';
+import {IconButton} from '@material-tailwind/react';
 
 const GradeBookCard = ({item}) => {
+  const axiosPrivate = useAxiosPrivate();
+
+  const deleteBook = async () => {
+    try {
+      const id = item?._id;
+      if (!id) return;
+      const response = await axiosPrivate.delete(`classmaterials/gradebook/${id}`);
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <>
-      <div className='max-w-xs p-4 rounded-md bg-gradient-to-b from-blue-500 via-blue-400 to-blue-600 shadow-md text-white dark:bg-gray-900 dark:text-gray-50'>
+      <div className='max-w-xs p-4 rounded-md bg-gradient-to-b from-blue-500 via-blue-400 to-blue-600 shadow-md text-white dark:bg-gray-900 dark:text-gray-50 relative'>
+        {/* <div className='absolute right-2 top-2'>
+          <IconButton onClick={deleteBook}></IconButton>
+          <FontAwesomeIcon icon={faEllipsisV} style={{color: 'black'}} />{' '}
+        </div> */}
         <img
           src={item?.coverPage?.url || 'https://source.unsplash.com/random/300x300/?book'}
           alt=''
